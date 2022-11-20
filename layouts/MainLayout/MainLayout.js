7a35c74a43b5b6f2b4896838ba0a9d84bae89dca
Yosef Berezovskiy <87656348+russianambassador@users.noreply.github.com>
2022-11-20 00:12:11 +0000

    feature: removed duplicate usemeqiaquery wobbling about the src directory. renamed meqiaQuery.js to useMediaQuery.js as it is a hook

--- layouts/MainLayout/MainLayout.js
+++ layouts/MainLayout/MainLayout.js
@@ -1,7 +1,7 @@
-import { Outlet } from "react-router-dom"
-import {useState, useEffect} from "react"
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { useMediaQuery } from "../../functions/mediaQuery";
-import "./MainLayout.scss"
+import "./MainLayout.scss";
 import RightBar from "../../components/RightBar/HomeRightBar/HomeRightBar";
 import LeftBar from "../../components/LeftBar/LeftBar";
 import TopBar from "../../components/TopBar/TopBar";
@@ -10,24 +10,21 @@ const MainLayout = () => {
   // console.log(useMediaQuery('(min-width: 460px)'))
   return (
     <div id="outer-container">
-      {useMediaQuery('(max-width: 495px)')?
-      <MobileRightBar pageWrapId={'page-wrap'} outerContainerId={'outer-container'}/>
-      :
-      null
-      }
+      {useMediaQuery("(max-width: 495px)") ? (
+        <MobileRightBar
+          pageWrapId={"page-wrap"}
+          outerContainerId={"outer-container"}
+        />
+      ) : null}
       <div id="page-wrap">
-        <TopBar/>
+        <TopBar />
         <div className="homeContainer">
-          {useMediaQuery('(min-width: 495px)')?
-          <LeftBar/>
-          :
-          null}
-          <Outlet/>
-          
+          {useMediaQuery("(min-width: 495px)") ? <LeftBar /> : null}
+          <Outlet />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
